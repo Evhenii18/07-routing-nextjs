422692129652176dcf8cee2158181ac67b118ffb
Evhenii18 <baranik18@gmail.com>
2025-08-30 14:21:04 +0100

    ED

--- app/notes/filter/@modal/(..)notes/[id]/page.tsx
+++ app/notes/filter/@modal/(..)notes/[id]/page.tsx
@@ -2,7 +2,13 @@
 
 import NotePreview from "@/components/NotePreview/NotePreview";
 
-export default function NotePage({ params }: { params: { id: string } }) {
+interface NotePageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function NotePage({ params }: NotePageProps) {
   return (
     <div className="max-w-2xl mx-auto p-6">
       <NotePreview id={params.id} />
